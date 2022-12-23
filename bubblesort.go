884c3c0a4d92ffe088415c23cbde2b045bd4d203
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-12-23 19:09:40 +0900

    update

--- bubblesort.go
+++ bubblesort.go
@@ -4,25 +4,24 @@ import "fmt"
 
 // 並び替えが発生するとき最小単位で考える 一気に考えようとしない
 
-func BubbleSort(nums []int) []int {
-
-	lenNumbers := len(nums) // 5
-
-	for i := 0; i < lenNumbers; i++ {
-		// 前提: 配列は0, 1, 2, 3, 4と数える
-		for j := 0; j < lenNumbers-1-i; j++ { // -1ずつずらしていけばOK 配列は0,1,2,3,4とアクセスすることに注意
-			// 0番目 0+1番目で左と右を比較
-			if nums[j] > nums[j+1] { // 手前の方が大きければ入れ替える
-				nums[j], nums[j+1] = nums[j+1], nums[j] // 0の部分を0+1にする のと 0+1の部分を0にする こうすることで数字を一気に入れ替える
+func bubblesort(nums []int) []int {
+	// まずは繰り返す回数を決定 -> 配列の要素分
+	for i := 0; i < len(nums); i++ {
+		// jは動かす回数 配列は0からアクセスするので1を引く、そしてどんどん順に左に詰めるので-iをする
+		for j := 0; j < len(nums)-1-i; j++ {
+			// 配列の0番目と0番目+1を比較する
+			if nums[j] > nums[j+1] {
+				// 左の方が大きければ入れ替える 例えば 10 > 8
+				nums[j], nums[j+1] = nums[j+1], nums[j]
 			}
 		}
-
 	}
-
 	return nums
 }
 
+// 問題: 昇順で並び替えて出力
+// 前提: スライスは参照型
 func main() {
-	nums := []int{2, 6, 1, 9, 7} // 要素数は5
-	fmt.Println(BubbleSort(nums))
+	nums := []int{7, 2, 9, 3, 6, 7, 8} // 要素数 -> 7
+	fmt.Println(bubblesort(nums))
 }
