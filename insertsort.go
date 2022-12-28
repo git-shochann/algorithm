0f9301a7b9d14115f470d26b1d4de8af465208b4
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-12-28 16:11:08 +0900

    wip insertsort

--- insertsort.go
+++ insertsort.go
@@ -5,21 +5,15 @@ import "fmt"
 func insertsort(nums []int) []int {
 	// 整列していない部分から挿入する値を取り出すループ i
 	for i := 1; i < len(nums); i++ {
-		temp := nums[i] // 整列していない最初の数値
+		temp := nums[i] // 整列していない動かしたい要素
 
-		// 整列済みと比較するループ j
-		// どんどん整列済みデータを右から左に見ていくため、indexがj--と減り、indexが0まで見ていく
+		// 整列済みの全ての要素と比較するループ j
+		// どんどん整列済みデータを右から左に見ていくため、indexがj--と減り、indexが0まで繰り返す
 		// j := i - 1 は整列済みデータの一番右を示す
-		var j int
-		for j = i - 1; j >= 0; i-- {
-			// 例えば7>2であれば
-			// 例えば2>8であれば入れ替えない？
-			if nums[j] > temp {
-				// ７を右にずらす
-				nums[j+1] = nums[j]
-			} else {
-				break
-			}
+		j := i - 1
+		for j >= 0 && nums[j] > temp {
+			nums[j+1] = nums[j]
+			j--
 		}
 		// ずらす処理が終わった位置に、挿入する値を入れる
 		// なぜj+1？
