88056b3c783cd51105ce44d67a722b6c4ca0bb81
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-12-28 15:59:16 +0900

    wip insertsort

--- insertsort.go
+++ insertsort.go
@@ -3,18 +3,32 @@ package main
 import "fmt"
 
 func insertsort(nums []int) []int {
+	// 整列していない部分から挿入する値を取り出すループ i
 	for i := 1; i < len(nums); i++ {
-		temp := nums[i]
-		// 0番目の要素と1番目の要素を比べる -> 0番目の方が大きければ、1番目と入れ替える
-		for i >= 0 && nums[i-1] > temp {
-			nums[i-1] = nums[i]
-			i++
+		temp := nums[i] // 整列していない最初の数値
+
+		// 整列済みと比較するループ j
+		// どんどん整列済みデータを右から左に見ていくため、indexがj--と減り、indexが0まで見ていく
+		// j := i - 1 は整列済みデータの一番右を示す
+		var j int
+		for j = i - 1; j >= 0; i-- {
+			// 例えば7>2であれば
+			// 例えば2>8であれば入れ替えない？
+			if nums[j] > temp {
+				// ７を右にずらす
+				nums[j+1] = nums[j]
+			} else {
+				break
+			}
 		}
+		// ずらす処理が終わった位置に、挿入する値を入れる
+		// なぜj+1？
+		nums[j+1] = temp
 	}
 	return nums
 }
 
 func main() {
-	nums := []int{7, 2, 9, 3, 0, 7, 8}
+	nums := []int{7, 2, 8, 4, 0}
 	fmt.Println(insertsort(nums))
 }
---
+++ shellsort.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func shellsort(nums []int) []int {
+
+}
+
+func main() {
+	nums := []int{7, 2, 9, 3, 6, 7, 8} // 要素数 -> 7
+	fmt.Println(shellsort(nums))
+}
