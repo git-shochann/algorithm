c2e2281fc09d7791fd8fff23605c90ec14b88297
git-shochann <sho_825.sa216@docomo.ne.jp>
2023-01-03 23:58:42 +0900

    wip

--- searcharg/sequentialsearch.go
+++ searcharg/sequentialsearch.go
@@ -1,7 +1,14 @@
 package main
 
+import "fmt"
+
 // リニアサーチ 先頭から順番に値を探す方法
 
-func main() {
+func sequentialsearch(nums []int) int {
 
+	return num
+}
+
+func main() {
+	fmt.Println(sequentialsearch([]int{2, 4, 5, 7, 1}))
 }
